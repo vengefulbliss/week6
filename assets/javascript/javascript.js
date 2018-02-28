80fa07dbfbadfe1c017d7f977bd0eeb2473dca5c
vengefulbliss <zapyaplee@gmail.com>
2018-02-27 18:14:11 -0700

    another

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,9 @@
+$(document ).ready(function)() {
+	// an array of actions, new actions will be pushed into this array;
+	var actions = ["spongebob", "wow", "nice", "crying", "cooking", "eating", "jumping", "hunting"];
+	//methods, and functions
+	// function for the buttons
+	function displayGifButtons(){
+		$("#gifButtonsView").empty(); //erase all the things in the div id to stop duplication.
+	}
+}
